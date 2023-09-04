ee21e8858c2504ce221f2bf00ef3e78c3ba9fdea
Corneliu <corneliu.rotari>
2023-09-04 18:32:56 +0300

    Update nvim custom file path

--- install.sh
+++ install.sh
@@ -19,5 +19,5 @@ ln_if "$DOT/zsh/env.zsh" ~/.zshenv
 # MODULES
 ln_if "$DOT/modules/nvm" ~/.nvm
 ln_if "$DOT/modules/nvim" ~/.config/nvim
-ln_if "$DOT/nvim" "$DOT/modules/nvim/custom"
+ln_if "$DOT/nvim" "$DOT/modules/nvim/lua/custom"
 ln_if "$DOT/i3" ~/.config/i3
