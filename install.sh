bceecca3a836af724c6837f708343bd80aa5fa0c
corneliu.rotari <corneliu.rotari>
2023-09-14 16:44:47 +0300

    Added tpm to install as a module

--- install.sh
+++ install.sh
@@ -59,6 +59,7 @@ conf_ln ()
   ln_if "$DOT/nvim" "$DOT/modules/nvim/lua/custom"
   ln_if "$DOT/i3" ~/.config/i3
   ln_if "$DOT/tmux" ~/.config/tmux
+  ln_if "$DOT/modules/tpm" "$DOT/tmux/plugins/tpm"
 }
 
 conf_nvm ()
