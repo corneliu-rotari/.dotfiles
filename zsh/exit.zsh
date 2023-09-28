f5e373affc4f5dd057285dade7f1c2674beb11b3
corneliu.rotari <corneliu.rotari>
2023-09-28 13:05:20 +0300

    Generic save after exit.

--- zsh/exit.zsh
+++ zsh/exit.zsh
@@ -1,11 +1,13 @@
 source "$DOT/zsh/functions.zsh"
 
 exit_handler() {
-	if ! check_wsl; then
-		save_gnome
-	fi
-	cd "$DOT" || exit
-	if [ -n "$(git status --porcelain)" ]; then
-		save_dotfiles "Generic save after exit."
+	if check_input "Save on exit?"; then
+		if ! check_wsl; then
+			save_gnome
+		fi
+		cd "$DOT" || exit
+		if [ -n "$(git status --porcelain)" ]; then
+			save_dotfiles "Generic save after exit."
+		fi
 	fi
 }
