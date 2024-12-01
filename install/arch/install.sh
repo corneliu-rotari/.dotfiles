1d08eb8b8709cb4d2aca33ed6cc4c6c817ae03a1
conrel <corneliu.rotari99@gmail.com>
2024-12-01 06:36:36 +0200

    Added to creation of symlinks and added some new pkgs

--- install/arch/install.sh
+++ install/arch/install.sh
@@ -1,8 +1,23 @@
-#!/bin/sh
+#!/bin/bash
 
-packages=(`cat packages.txt`)
+if [[ $UID -eq 0 ]]; then
+  echo "User root not going to run it"
+  exit 1
+fi
 
+DOTFILES="$HOME/.dotfiles"
+
+packages=($(cat ~/.dotfiles/install/arch/packages.txt))
 
 for package in "${packages[@]}"; do
-    echo "Installing $package"
+    printf "%-70s [OK]\n" "Installing $package"
+    sudo pacman -S --needed "$package"
 done
+
+# Create sym links for XDG_CONFIG_HOME
+if [[ -n $XDG_CONFIG_HOME ]]; then
+  for link in "$DOTFILES/config"/*; do
+    printf "%-70s [OK]\n" "Created link $link"
+    ln -sf "$link" "$XDG_CONFIG_HOME/$(basename "$link")" 
+  done
+fi
